d4d798ee9905e6c6c81b8d06c34dff81b053f0cd
Leticia <leticiarp2000@hotmail.com>
2024-01-25 09:37:48 -0300

    chore: launch for robot description

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -6,6 +6,8 @@ import launch_ros
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import launch_ros.descriptions
+
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='fred2_description').find('fred2_description')
@@ -15,8 +17,8 @@ def generate_launch_description():
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-    )
+        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue( launch.substitutions.Command(['xacro ',os.path.join(pkg_share,'src/description/fred_description.urdf')]), value_type=str)  }]    )
+    
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
@@ -45,6 +47,8 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
