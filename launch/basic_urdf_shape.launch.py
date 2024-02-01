d9456029362eacb24c62478a8e3f9ffb885d9e0c
Leticia <leticiarp2000@hotmail.com>
2024-02-01 10:48:34 -0300

    chore: launch for only robot_descriptor

---
+++ launch/basic_urdf_shape.launch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import launch_ros.descriptions
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='fred2_description').find('fred2_description')
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue( launch.substitutions.Command(['xacro ',os.path.join(pkg_share,'src/description/fred_basic_shape.urdf')]), value_type=str)  }]    )
+    
+
+    return launch.LaunchDescription([
+
+        robot_state_publisher_node,
+        
+    ])
